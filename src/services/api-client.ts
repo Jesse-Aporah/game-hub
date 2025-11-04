ca3bc8b3e3c8a1156f2eb2f165018c1a24c5eb7a
Jesse Aporah <jesseaporah6@gmail.com>
2025-11-04 15:06:00 +0100

    finished project

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -3,6 +3,6 @@ import axios from "axios";
 export default axios.create({
   baseURL: "https://api.rawg.io/api",
   params: {
-    key: "c7b18323a47d40c394ea5b019646b1f5",
+    key: "89304be12ead40de8e6bb29ead8608ce",
   },
 });
